b04e80148135567974dfdc13f63ef366038d84a9
sophia <sophieoyiza@gmail.com>
2021-10-10 23:47:46 +0100

    feat: :sparkles: update
    update
    
    none

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 let main = document.getElementById('main');
 let menuDesktop = document.getElementById('menu-desktop');
 
-const menuToggle = () =>{
-    console.log('menuToggle');
-    if (main.style.display === 'flex'){
-        menuDesktop.style.display = 'flex';
-        main.style.display = 'none';
-       
-    } else {
+const menuOpen = () =>{
+    console.log('menu open');
+    main.style.display = 'none';
+    menuDesktop.style.display = 'flex';
+    
+}
+
+const menuClose = () =>{
+    console.log('menu close');
         main.style.display = 'flex';
         menuDesktop.style.display = 'none';
-    }
+    
 }
 
 
